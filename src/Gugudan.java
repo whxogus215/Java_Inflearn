7bbccc69dbd8148c64aa183fa269303e70dc34fa
whxogus215 <whxogus215@naver.com>
2022-01-05 23:26:01 +0900

    2022-01-05

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,4 +1,43 @@
+import java.util.Scanner;
 
 public class Gugudan {
+	public static void main(String[] args) {
+		// 2단
+//		System.out.println(2 * 1);
+//		System.out.println(2 * 2);
+//		System.out.println(2 * 3);
+//		System.out.println(2 * 4);
+//		System.out.println(2 * 5);
+		
+		//3단
+//		System.out.println(3 * 1);
+//		System.out.println(3 * 2);
+//		System.out.println(3 * 3);
+//		System.out.println(3 * 4);
+//		System.out.println(3 * 5);
+		
+//		//4단
+//		System.out.println("4단");
+//		int result = 4 * 1;
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+		
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
+		
+		
+		
+	}
 
 }
