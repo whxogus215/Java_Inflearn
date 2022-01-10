cafa629647f441b16f66d1981ab280061ac2cc5c
whxogus215 <whxogus215@naver.com>
2022-01-11 08:47:32 +0900

    2022-01-11(메소드 활용해 구구단 다시 구현까지)

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 
 public class Gugudan {
-	public static void main(String[] args) {
+	public static int[] calculate(int times) {
 		int[] result = new int[9];
-		
-		for(int j = 2;j < 10;j++) {
-			for(int i =0; i<result.length; i++) {
-				result[i] = j * (i+1);
-			}
-			for(int i =0; i < result.length; i++) {
-				System.out.println(result[i]);
-			}
-			System.out.println();
+		for(int i =0; i < result.length; i++) {
+			result[i] = times * (i+1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i=0 ; i< result.length;i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		for(int i =2; i<10; i++) {
+			int[] result = calculate(i);
+			print(result);
+			
 		}
 		
 	}
 }
+
+
