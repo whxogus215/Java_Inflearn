57ef29d8889c0a4235766d11f7f414367f6d341f
whxogus215 <whxogus215@naver.com>
2022-01-11 22:38:27 +0900

    2022-01-11

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -15,14 +15,7 @@ public class Gugudan {
 		}
 	}
 	
-	public static void main(String[] args) {
-		for(int i =2; i<10; i++) {
-			int[] result = calculate(i);
-			print(result);
-			
-		}
-		
-	}
+	
 }
 
 
